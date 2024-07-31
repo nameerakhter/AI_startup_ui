d3c05f8ff8c16ccb1ad0030b4f0930b3e85d0370
nameerakhter <akhtarnameer@gmail.com>
2024-07-31 20:55:53 +0530

    Navbar and custom globlas.css

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
+import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="bg-custom-background bg-cover bg-center min-h-screen">
+    <div className="bg-black">
       <Navbar />
-      {/* <Hero /> */}
+      <Hero />
     </div>
   );
 }
---
+++ components/Hero.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='bg-custom-background bg-cover min-h-screen'>Hero</div>
+  )
+}
+
+export default Hero
