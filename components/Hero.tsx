c3bc6fe185f5c2f0aa727bdb10a0f491878e0a6a
nameerakhter <akhtarnameer@gmail.com>
2024-08-01 14:31:01 +0530

    Hero section completed

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,10 @@
+'use client'
 import React from "react";
 import Badge from "./elements/Badge";
 import { Button } from "./ui/button";
 import Image from "next/image";
+import { motion } from "framer-motion"
+
 
 const Hero = () => {
   return (
@@ -9,17 +12,23 @@ const Hero = () => {
       <section className="remove-scrollbar container mt-[2rem]">
         <div className="sub-container flex flex-col py-10 items-center">
           <Badge />
-          <div className="flex flex-col items-center justify-center py-[2rem] gap-3">
+          <div className="flex flex-col items-center justify-center py-[2rem] gap-3 ">
             <h1 className="text-7xl ">Boost your</h1>
             <h1 className="text-7xl ">rankings with AI.</h1>
             <p>Elevate your site’s visibility effortlessly with AI, where </p>
             <p>smart technology meets user-friendly SEO tools.</p>
           </div>
-        <Button>Start for free</Button>
+          <motion.div whileHover={{ scale: 1.1 }} transition={{ delay: .1 }}>
+            <Button>Start for free</Button>
+          </motion.div>
         </div>
       </section>
       <div className="flex flex-col items-center justify-center">
-      <Image src='/assets/Images/app-widget-1.png' alt="app-widget" height={794} width={1098} className=""/>
+        <div className="relative w-full flex flex-col items-center">
+        <Image src='/assets/Images/app-widget-1.png' alt="app-widget" height={794} width={1098} />
+        <div className="w-full h-[400px] absolute bottom-0 bg-gradient-to-t from-black to-transparent" ></div>
+        </div>
+      
       </div>
     </div>
   );
--- components/elements/Badge.tsx
+++ components/elements/Badge.tsx
@@ -1,13 +1,15 @@
+'use client'
 import React from 'react'
+import { motion } from "framer-motion"
 
 const Badge = () => {
   return (
-    <div className='bg-black  rounded-full thin-border2'>
+    <motion.div className='bg-black  rounded-full thin-border2' whileHover={{ scale: 1.1 }} transition={{ delay: .1 }}>
       <div className='px-2 flex items-center gap-3 '>
         <p className='px-2 bg-purple-light rounded-full text-black font-extrabold text-sm'>New</p>
         <p className='text-purple-light'>Latest Integration just arrived</p>
       </div>
-    </div>
+    </motion.div>
   )
 }
 
