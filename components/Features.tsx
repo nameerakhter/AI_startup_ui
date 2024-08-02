fd2726f31fca21bf8a86d50aa9da2c1284d6d914
nameerakhter <akhtarnameer@gmail.com>
2024-08-02 12:23:19 +0530

    Update Features component with new design and layout

--- components/Features.tsx
+++ components/Features.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import FeatureDescription from "./elements/FeatureDescription";
 
 const Features = () => {
   return (
-    <div>Features</div>
-  )
-}
+    <div className="relative bg-gradient-to-t from-purple-950 to-transparent p-4 min-h-screen top-2">
+      <div className="absolute flex flex-col ">
+        <h1 className="px-[5rem] text-[5rem] capitalize">
+         Elevate your 
+        </h1>
+        <h1 className="px-[5rem] text-[5rem] capitalize">
+        SEO efforts.
+        </h1>
+      </div>
+      <div className="absolute w-full bg-red-700 h-[600px] bottom-[5rem] p-[4rem] flex flex-row items-center gap-4">
+        <section className="w-1/3 bg-red-300 h-[600px]">
+        <FeatureDescription/></section>
+        <section className="w-1/3 bg-red-400 h-[600px]"></section>
+        <section className="w-1/3 h-[600px]"></section>
+      </div>
+    </div>
+  );
+};
 
-export default Features
+export default Features;
---
+++ components/elements/FeatureDescription.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FeatureDescription = () => {
+  return (
+    <div>FeatureDescription</div>
+  )
+}
+
+export default FeatureDescription
